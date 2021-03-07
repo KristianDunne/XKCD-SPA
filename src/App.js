b9fa52b3e1b54e89156d6a6dec658c3fd8f0c2e7
Kristian Dunne <KristianDunne@icloud.com>
2021-03-07 18:35:41 +0000

    Add a form that allows the user to change comic by id

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./App.css";
 import Comic from "./components/Comic";
+import { useState } from "react";
 
 function App() {
   const latestComicUrl = "https://xkcd.now.sh/?comic=latest";
+  const [comicUrl, setComicUrl] = useState(latestComicUrl);
+  const [comicID, setComicID] = useState("");
+
+  const handleChange = (e) => {
+    setComicID(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setComicUrl(`https://xkcd.now.sh/?comic=${comicID}`);
+  };
 
   return (
     <div className="App">
       <h1>XKCD SPA</h1>
-      <Comic url={latestComicUrl} />
+      <form onSubmit={handleSubmit} data-testid="form">
+        <label>
+          Find a comic by ID:{" "}
+          <input
+            type="number"
+            name="name"
+            data-testid="input"
+            value={comicID}
+            onChange={handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" data-testid="submit" />
+      </form>
+      <Comic url={comicUrl} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,37 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+const component = (
+  <Provider store={store}>
+    <App></App>
+  </Provider>
+);
+
 describe("App component", () => {
   it("should render along with it's child components", () => {
-    render(
-      <Provider store={store}>
-        <App></App>
-      </Provider>
-    );
+    render(component);
     const titleElement = screen.getByText(/XKCD SPA/i);
     expect(titleElement).toBeInTheDocument();
+  });
+
+  it("should handle the input field change", () => {
+    render(component);
+    const input = screen.getByTestId("input");
+    fireEvent.change(input, { target: { value: "123" } });
+    expect(input.value).toBe("123");
+  });
+
+  it("should handle the form submit", () => {
+    render(component);
+    const input = screen.getByTestId("input");
+    const form = screen.getByTestId("form");
+    const onSubmit = jest.fn();
+    fireEvent.change(input, { target: { value: "123" } });
+    fireEvent.submit(form);
+    waitFor(() => {
+      expect(onSubmit).toHaveBeenCalled();
+    }).then();
   });
 });
